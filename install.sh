a43989aee123545740035abfb4d05a4d41108901
Christoph Werner <christoph@codepunkt.de>
2018-11-15 09:13:32 +0100

    fix: install fn

--- install.sh
+++ install.sh
@@ -36,161 +36,165 @@ spinner_stop() {
   echo -e "$checkmark\033[?25h"
 }
 
-# store current user
-THIS_USER=`pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 's/[()]//g'`
+install() {
+  # store current user
+  THIS_USER=`pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1 | sed 's/[()]//g'`
 
-# require sudo privileges
-if [ $EUID != 0 ]; then
-  sudo "$0" "$@"
-  exit $?
-fi
+  # require sudo privileges
+  if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+  fi
 
-# update packages
-spinner_start "updating packages"
-sudo apt update -qq &> /dev/null && \
-  sudo apt dist-upgrade -y -qq &> /dev/null && \
-  sudo apt autoremove -y -qq &> /dev/null && \
-  sudo apt autoclean -y -qq &> /dev/null
-spinner_stop
+  # update packages
+  spinner_start "updating packages"
+  sudo apt update -qq &> /dev/null && \
+    sudo apt dist-upgrade -y -qq &> /dev/null && \
+    sudo apt autoremove -y -qq &> /dev/null && \
+    sudo apt autoclean -y -qq &> /dev/null
+  spinner_stop
 
-# install packages
-spinner_start "installing additional packages"
-sudo apt -y -qq install \
-  apt-transport-https \
-  build-essential \
-  ca-certificates \
-  curl \
-  git \
-  jq \
-  python-pip \
-  software-properties-common \
-  zsh \
-  &> /dev/null
-spinner_stop
+  # install packages
+  spinner_start "installing additional packages"
+  sudo apt -y -qq install \
+    apt-transport-https \
+    build-essential \
+    ca-certificates \
+    curl \
+    git \
+    jq \
+    python-pip \
+    software-properties-common \
+    zsh \
+    &> /dev/null
+  spinner_stop
 
-# dotfiles
-if [ -d ~/.dotfiles ]; then
-  spinner_start "pulling dotfile updates"
-  cd ~/.dotfiles && \
-    git pull &> /dev/null && \
-    cd -
-  spinner_stop
-else
-  spinner_start "cloning dotfiles"
-  git clone https://github.com/codepunkt/dotfiles-win.git ~/.dotfiles &> /dev/null
-  spinner_stop
-fi
+  # dotfiles
+  if [ -d ~/.dotfiles ]; then
+    spinner_start "pulling dotfile updates"
+    cd ~/.dotfiles && \
+      git pull &> /dev/null && \
+      cd -
+    spinner_stop
+  else
+    spinner_start "cloning dotfiles"
+    git clone https://github.com/codepunkt/dotfiles-win.git ~/.dotfiles &> /dev/null
+    spinner_stop
+  fi
 
-# ssh key
-pubKey="$HOME/.ssh/id_rsa"
-if [ ! -f "$pubKey" ]; then
-  spinner_start "creating ssh key"
-  ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa  &> /dev/null
-  spinner_stop
-else
-  echo "$checkmark ssh key exists"
-fi
+  # ssh key
+  pubKey="$HOME/.ssh/id_rsa"
+  if [ ! -f "$pubKey" ]; then
+    spinner_start "creating ssh key"
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa  &> /dev/null
+    spinner_stop
+  else
+    echo "$checkmark ssh key exists"
+  fi
 
-# docker
-if [ -x "$(command -v docker)" ]; then
-  echo "$checkmark docker is installed"
-else
-  spinner_start "installing docker"
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &> /dev/null && \
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &> /dev/null && \
-    sudo apt update -qq &> /dev/null && \
-    sudo apt install docker-ce -y -qq &> /dev/null
-  spinner_stop
-fi
+  # docker
+  if [ -x "$(command -v docker)" ]; then
+    echo "$checkmark docker is installed"
+  else
+    spinner_start "installing docker"
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &> /dev/null && \
+      sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &> /dev/null && \
+      sudo apt update -qq &> /dev/null && \
+      sudo apt install docker-ce -y -qq &> /dev/null
+    spinner_stop
+  fi
 
-# oh-my-zsh
-if [ -d ~/.oh-my-zsh ]; then
-  spinner_start "pulling oh-my-zsh updates"
-  cd ~/.oh-my-zsh && \
-    git pull &> /dev/null && \
-    cd - &> /dev/null
-  spinner_stop
-else
-  spinner_start "installing oh-my-zsh"
-  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
-  spinner_stop
-fi
+  # oh-my-zsh
+  if [ -d ~/.oh-my-zsh ]; then
+    spinner_start "pulling oh-my-zsh updates"
+    cd ~/.oh-my-zsh && \
+      git pull &> /dev/null && \
+      cd - &> /dev/null
+    spinner_stop
+  else
+    spinner_start "installing oh-my-zsh"
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
+    spinner_stop
+  fi
 
-# zsh syntax highlighting
-if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-  spinner_start "pulling zsh-syntax-highlighting updates"
-  cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && \
-    git pull &> /dev/null && \
-    cd - &> /dev/null
-  spinner_stop
-else
-  spinner_start "installing zsh-syntax-highlighting"
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-  spinner_stop
-fi
+  # zsh syntax highlighting
+  if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    spinner_start "pulling zsh-syntax-highlighting updates"
+    cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && \
+      git pull &> /dev/null && \
+      cd - &> /dev/null
+    spinner_stop
+  else
+    spinner_start "installing zsh-syntax-highlighting"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    spinner_stop
+  fi
 
-# nvm
-if [ -d ~/.dotfiles ]; then
-  spinner_start "updating nvm"
-  cd ~/.nvm && \
-    git pull &> /dev/null && \
-    cd - &> /dev/null
-  spinner_stop
-else
-  spinner_start "installing nvm"
-  curl -s -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash &> /dev/null
-  spinner_stop
-fi
+  # nvm
+  if [ -d ~/.dotfiles ]; then
+    spinner_start "updating nvm"
+    cd ~/.nvm && \
+      git pull &> /dev/null && \
+      cd - &> /dev/null
+    spinner_stop
+  else
+    spinner_start "installing nvm"
+    curl -s -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash &> /dev/null
+    spinner_stop
+  fi
 
-# node
-if [ -x "$(command -v docker)" ]; then
-  spinner_start "updating node"
-  sudo -Hu ${THIS_USER} bash -c '. ~/.nvm/nvm.sh && nvm i --lts --latest-npm' &> /dev/null
-  spinner_stop
-else
-  spinner_start "installing node"
-  nvm i --lts --latest-npm &> /dev/null
-  spinner_stop
-fi
+  # node
+  if [ -x "$(command -v docker)" ]; then
+    spinner_start "updating node"
+    sudo -Hu ${THIS_USER} bash -c '. ~/.nvm/nvm.sh && nvm i --lts --latest-npm' &> /dev/null
+    spinner_stop
+  else
+    spinner_start "installing node"
+    nvm i --lts --latest-npm &> /dev/null
+    spinner_stop
+  fi
 
-# npm packages
-spinner_start "installing npm packages"
-sudo -Hu ${THIS_USER} bash -c '. ~/.nvm/nvm.sh && npm i -g yarn create-react-app np npm-name-cli tldr ndb yo diff-so-fancy fkill-cli' &> /dev/null
-spinner_stop
-
-# symlink config files
-spinner_start "symlinking config files"
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-spinner_stop
-
-# alias windows programs
-if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null; then
-  spinner_start "symlinking windows programs"
-  mkdir -p ~/.bin
-  ln -sf /mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe ~/.bin/chrome
+  # npm packages
+  spinner_start "installing npm packages"
+  sudo -Hu ${THIS_USER} bash -c '. ~/.nvm/nvm.sh && npm i -g yarn create-react-app np npm-name-cli tldr ndb yo diff-so-fancy fkill-cli' &> /dev/null
   spinner_stop
-fi
 
-# bat
-if [ -x "$(command -v wat)" ]; then
-  echo "$checkmark bat is installed"
-else
-  spinner_start "installing bat"
-  wget -O ~/bat_0.9.0_amd64.deb https://github.com/sharkdp/bat/releases/download/v0.9.0/bat_0.9.0_amd64.deb &> /dev/null
-  sudo dpkg -i ~/bat_0.9.0_amd64.deb &> /dev/null
-  rm -f ~/bat_0.9.0_amd64.deb &> /dev/null
+  # symlink config files
+  spinner_start "symlinking config files"
+  ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+  ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+  ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
   spinner_stop
-fi
 
-# z
-if [ -x "$(command -v z)" ]; then
-  echo "$checkmark z is installed"
-else
-  spinner_start "installing z"
-  wget -O ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh &> /dev/null
-  spinner_stop
-fi
+  # alias windows programs
+  if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null; then
+    spinner_start "symlinking windows programs"
+    mkdir -p ~/.bin
+    ln -sf /mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe ~/.bin/chrome
+    spinner_stop
+  fi
+
+  # bat
+  if [ -x "$(command -v wat)" ]; then
+    echo "$checkmark bat is installed"
+  else
+    spinner_start "installing bat"
+    wget -O ~/bat_0.9.0_amd64.deb https://github.com/sharkdp/bat/releases/download/v0.9.0/bat_0.9.0_amd64.deb &> /dev/null
+    sudo dpkg -i ~/bat_0.9.0_amd64.deb &> /dev/null
+    rm -f ~/bat_0.9.0_amd64.deb &> /dev/null
+    spinner_stop
+  fi
+
+  # z
+  if [ -x "$(command -v z)" ]; then
+    echo "$checkmark z is installed"
+  else
+    spinner_start "installing z"
+    wget -O ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh &> /dev/null
+    spinner_stop
+  fi
+}
+
+install
 
 } # this ensures the entire script is downloaded #
